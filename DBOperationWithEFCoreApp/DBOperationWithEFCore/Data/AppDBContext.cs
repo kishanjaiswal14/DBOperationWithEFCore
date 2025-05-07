524c8b83a7d60fceeef01b32309a6ffbe25fe40c
Kishan Jaiswal <kishanjasiwal1402@gmail.com>
2025-05-07 20:37:52 +0530

    Installed EFcore and setup the db context

---
+++ DBOperationWithEFCoreApp/DBOperationWithEFCore/Data/AppDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DBOperationWithEFCore.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options)
+        {
+            
+        }
+    }
+}
